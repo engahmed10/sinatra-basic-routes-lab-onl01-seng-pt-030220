143b88174f64e5ba31d1da3737b998417215049a
AHMED AFANDY <eng.computer1@yahoo.com>
2020-05-13 04:00:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,13 +2,6 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-
-  get '/name' do
-      "My name is Ahmed Afandy"
-  end
-  get  '/hometown'  do
-    "My hometown is earth"
-  end
   get  '/favorite-song'  do
    "My favorite song is Aylin"
   end
